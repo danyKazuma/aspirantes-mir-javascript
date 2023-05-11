895f7ca95fd5607eac9b945eca50f8b2598cefe0
danyKazuma <31149888+danyKazuma@users.noreply.github.com>
2023-05-10 19:29:00 -0500

    solucion ejercicios javascript

---
+++ js-1/hello.js
@@ -0,0 +1,9 @@
+// escribe la función hola acá
+function hola(nombre) {
+    var saludo="Hola "+nombre+"!";
+    return saludo;
+  }
+// código de prueba
+console.log(hola("Pedro")) // "Hola Pedro!"
+console.log(hola("Juan")) // "Hola Juan!"
+console.log(hola("")) // "Hola !"
---
+++ js-1/sum.js
@@ -0,0 +1,9 @@
+// escribe la función suma acá
+function sum(numero){ 
+    numero=numero*(numero+1)/2;
+    return numero;
+}
+// código de prueba
+console.log(sum(4)) // 1 + 2 + 3 + 4 = 10
+console.log(sum(10)) // 55
+console.log(sum(15)) // 120
